48eba569ea17e18b70d501faea321cf2dd7da4ae
tk1351 <takakikitsui0111@gmail.com>
2024-09-05 17:07:27 +0900

    feat: add HelloParams

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use axum::response::Html;
+use axum::extract::Query;
+use axum::response::{Html, IntoResponse};
 use axum::routing::get;
 use axum::Router;
+use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/hello", get(|| async { Html("<h1>Hello World!</h1>") }));
+    let app = Router::new().route("/hello", get(handler_hello));
 
     // start server
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
@@ -12,4 +14,16 @@ async fn main() {
         .unwrap();
     println!("->> LISTENING on {}\n", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
+}
+
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>,
+}
+
+async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello - {params:?}", "HANDLER");
+
+    let name: &str = params.name.as_deref().unwrap_or("World!");
+    Html(format!("<h1>Hello {name}!</h1>"))
 }
--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -5,6 +5,7 @@ async fn quick_dev() -> Result<()> {
     let hc = httpc_test::new_client("http://localhost:8080")?;
 
     hc.do_get("/hello").await?.print().await?;
+    hc.do_get("/hello?name=Bob").await?.print().await?;
 
     Ok(())
 }
