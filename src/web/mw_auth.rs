db7aec8e4d6efbe8ad7bdc5691a5bae3e9baa329
tk1351 <takakikitsui0111@gmail.com>
2024-09-06 18:44:51 +0900

    feat: add context

--- src/web/mw_auth.rs
+++ src/web/mw_auth.rs
@@ -1,23 +1,43 @@
+use async_trait::async_trait;
 use axum::body::Body;
+use axum::extract::FromRequestParts;
+use axum::http::request::Parts;
 use axum::http::Request;
 use axum::middleware::Next;
 use axum::response::Response;
+use axum::RequestPartsExt;
 use lazy_regex::regex_captures;
 use tower_cookies::Cookies;
 
+use crate::ctx::Ctx;
 use crate::web::AUTH_TOKEN;
 use crate::{Error, Result};
 
-pub async fn mw_require_auth(cookies: Cookies, req: Request<Body>, next: Next) -> Result<Response> {
-    println!("->> {:<12} - mw_require_auth", "MIDDLEWARE");
+pub async fn mw_require_auth(ctx: Result<Ctx>, req: Request<Body>, next: Next) -> Result<Response> {
+    println!("->> {:<12} - mw_require_auth - {ctx:?}", "MIDDLEWARE");
 
-    let auth_token = cookies.get(AUTH_TOKEN).map(|c| c.value().to_string());
-
-    let (user_id, exp, sign) = auth_token
-        .ok_or(Error::AuthFailNoAuthTokenCookie)
-        .and_then(parse_token)?;
+    ctx?;
 
     Ok(next.run(req).await)
+}
+
+#[async_trait]
+impl<S: Send + Sync> FromRequestParts<S> for Ctx {
+    type Rejection = Error;
+
+    async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self> {
+        println!("->> {:<12} - Ctx", "EXTRACTOR");
+
+        let cookies = parts.extract::<Cookies>().await.unwrap();
+
+        let auth_token = cookies.get(AUTH_TOKEN).map(|c| c.value().to_string());
+
+        let (user_id, exp, sign) = auth_token
+            .ok_or(Error::AuthFailNoAuthTokenCookie)
+            .and_then(parse_token)?;
+
+        Ok(Ctx::new(user_id))
+    }
 }
 
 fn parse_token(token: String) -> Result<(u64, String, String)> {
